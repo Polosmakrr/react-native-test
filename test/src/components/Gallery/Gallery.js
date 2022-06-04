b2ebe7cde13dc0451f7b91f4d3d61d1fbd3378d2
Polosmakr <polosmakrr@gmail.com>
2022-06-04 18:54:59 +0300

    1

--- test/src/components/Gallery/Gallery.js
+++ test/src/components/Gallery/Gallery.js
@@ -1,17 +1,31 @@
-import { StyleSheet, View, Image, TouchableHighlight } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  TouchableHighlight,
+  Text,
+} from "react-native";
 
 export const Gallery = ({ data, choseImg }) => {
   return (
     <View style={styles.container}>
       {data.map((item, index) => (
         <TouchableHighlight onPress={() => choseImg(index)} key={item.id}>
-          <Image
-            key={item.id}
-            style={styles.img}
-            source={{
-              uri: `${item.urls.small}`,
-            }}
-          />
+          <View style={styles.container_img}>
+            <Image
+              key={item.id}
+              style={styles.img}
+              source={{
+                uri: `${item.urls.small}`,
+              }}
+            />
+            <View style={styles.description}>
+              <Text>Author:</Text>
+              <Text style={styles.name}>{item.user.name}</Text>
+              <Text>Description:</Text>
+              <Text style={styles.description_text}>{item.description}</Text>
+            </View>
+          </View>
         </TouchableHighlight>
       ))}
     </View>
@@ -32,6 +46,26 @@ const styles = StyleSheet.create({
   img: {
     width: 150,
     height: 150,
-    margin: 10,
+  },
+  container_img: {
+    width: 150,
+    justifyContent: "center",
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 10,
+    padding: 10,
+  },
+  name: {
+    fontSize: 12,
+    fontWeight: "bold",
+    paddingLeft: 4,
+    paddingBottom: 10,
+  },
+  description_text: {
+    paddingLeft: 4,
+    paddingTop: 10,
   },
 });
